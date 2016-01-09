1293d8ff13575bf6a17a14eeb0a5cc2f4d0e3753
mynameiscraziu <tarikia7@gmail.com>
2016-01-09 15:11:34 +0000

    Create cray.lua

---
+++ plugins/cray.lua
@@ -0,0 +1,20 @@
+do
+local function run(msg, matches)
+local bot_id = 129791651
+local fbotmain = 156539679
+
+    if matches[1] == 'bye' and is_admin(msg) or msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
+       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+    elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(fbotmain) then
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..fbotmain, ok_cb, false)
+    end
+end
+ 
+return {
+  patterns = {
+    "^[!/](bye)$",
+    "^!!tgservice (.+)$",
+  },
+  run = run
+}
+end
